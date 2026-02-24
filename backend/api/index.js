a36f1fd1e66de71a548915cdaedad8fa46503e86
Hitanshiee <161367204+Hitanshiee@users.noreply.github.com>
2026-02-24 11:01:43 +0530

    verificationwallfix

--- backend/api/index.js
+++ backend/api/index.js
@@ -107,7 +107,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'X-User-Email'],
+  allowedHeaders: '*',
   exposedHeaders: ['Content-Range', 'X-Content-Range']
 };
 
